69436a4091d61e02351684ee1a3aa969a7d039e5
Radhika Prajapat <radhikaprajapat9754@gmail.com>
2026-09-18 15:34:51 +0530

    Keep current backend and frontend

--- src/main/java/com/example/backend/exception/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-
-    public StudentNotFoundException(String message){
-        super(message);
-    }
-    
-}
--- src/main/java/com/example/backend/repo/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.models.CourseModel;
-
-public interface CourseRepo extends JpaRepository<CourseModel, Long> {
-    
-}
